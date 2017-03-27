4506cbaedd81b40d38fe376366c0159cbfa35eac
senem qenberova <senem qenberova>
2017-03-27 14:04:33 +0400

    commits

--- js/script.js
+++ js/script.js
@@ -1,2 +1,106 @@
-console.log(window.innerWidth);
-console.log(window.outerWidth);
+var first_slider = document.querySelector(".content .first_slider");
+var bg_img = ["(img/bg1.jpg)","(img/bg2.jpg)","(img/bg3.jpg)"];
+var prev1 = document.getElementById("prev1");
+var next1 = document.getElementById("next1");
+
+var click1 = 0;
+
+first_slider.style.backgroundImage = "url" + bg_img[click1];
+
+next1.addEventListener("click", function(){
+	click1++;
+	if(click1 >= bg_img.length){
+		click1 = 0;
+	}
+	first_slider.style.backgroundImage = "url" + bg_img[click1];
+});
+
+prev1.addEventListener("click", function(){
+	click1--;
+	if(click1 < 0){
+		click1 = bg_img.length - 1;
+	}
+	first_slider.style.backgroundImage = "url" + bg_img[click1];
+});
+// console.log();
+
+
+var buy = document.querySelector(".multi_purpose .more_template .buy_theme");
+var p_in_buy = buy.querySelector("p");
+
+buy.addEventListener("mouseover",function(){
+	p_in_buy.innerHTML = "YES I WANT IT";
+	p_in_buy.style.animation = "yes .3s "
+
+});
+
+buy.addEventListener("mouseout",function(){
+	p_in_buy.innerHTML = "BUY THIS THEME";
+	p_in_buy.style.animation = "buy_theme .3s "
+});
+
+// Second Slider
+var prev2 = document.getElementById("prev2");
+var next2 = document.getElementById("next2");
+var member_img = document.querySelectorAll(".team_member .member_img");
+var team_slider = document.querySelector(".outer_team .team_slider");
+
+click2 = 0;
+
+console.log(member_img);
+team_slider.style.transform = "translateX(0px);"
+
+next2.addEventListener("click",function() {
+	console.log("next");
+	click2++;
+	team_slider.style.transition = "all .4s ease-in-out";
+	if(click2 >= member_img.length - 2){
+		click2 = 0;
+		team_slider.style.transform = "translateX(390px);"
+	}
+	team_slider.style.transform += "translateX(-390px);"
+	console.log("click "+ click2);
+}); 
+
+prev2.addEventListener("click",function(){
+	console.log("previous");
+	var len = (-390) * (member_img.length - 2);  
+	click2--;
+	team_slider.style.transition = "all .4s ease-in-out";
+	if(click2 < 0){
+		click2 = member_img.length - 3;
+		team_slider.style.transform = "translateX(" + len + "px)";
+	}
+	team_slider.style.transform += "translateX(390px);"
+	console.log("click "+ click2);
+});
+
+var team_member = document.querySelectorAll(".team_member");
+var member_info = document.querySelectorAll(".member_info");
+var name_position = document.querySelectorAll(".name_position");
+var media_info = document.querySelectorAll(".media_info");
+console.log();
+
+for(var i = 0 ;i< member_info.length;i++){
+	member_info[i].addEventListener("mouseover", teamMemberFadeIn);
+	member_info[i].addEventListener("mouseout", teamMemberFadeOut);
+}
+
+function teamMemberFadeIn(event){
+	for(var i=0;i<team_member.length;i++){
+		this.style.opacity = 1;
+		this.style.transition = "all .5s linear";
+		name_position[i].style.animation = "name_position_fadeIn .4s ease-in-out";
+		media_info[i].style.animation = "media_info_fadeIn .4s ease-in-out";
+		console.log(this)
+	}
+}
+
+function teamMemberFadeOut(event){
+	for(var i=0;i<team_member.length;i++){
+		this.style.opacity = 0;
+		this.style.transition = "opacity .5s ease-in-out";
+		name_position[i].style.animation = "name_position_fadeOut .4s ease-in-out";
+		media_info[i].style.animation = "media_info_fadeOut .4s ease-in-out";
+	}
+}
